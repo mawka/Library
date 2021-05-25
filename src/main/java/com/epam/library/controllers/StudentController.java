2fece0dc604b2e7fd8401eeb14f67c79683ac9b8
Bugakova Mayia <50870319+mawka@users.noreply.github.com>
2021-05-24 23:24:35 -0700

    Add dto to Students

--- src/main/java/com/epam/library/controllers/StudentController.java
+++ src/main/java/com/epam/library/controllers/StudentController.java
@@ -1,16 +1,20 @@
 package com.epam.library.controllers;
 
+import com.epam.library.dto.StudentDto;
 import com.epam.library.model.Student;
 import com.epam.library.service.Implementation.StudentServiceImpl;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/students")
@@ -19,18 +23,39 @@ public class StudentController {
     @Autowired
     private StudentServiceImpl studentServiceImpl;
 
+    private ModelMapper modelMapper = new ModelMapper();
+
+    private StudentDto convertToDto(Student student) {
+        StudentDto studentDto = modelMapper.map(student, StudentDto.class);
+        return studentDto;
+    }
+
+    private Student convertToEntity(StudentDto studentDto) {
+        Student student = modelMapper.map(studentDto, Student.class);
+        return student;
+    }
+
     @GetMapping()
-    public List<Student> getAllStudents() {
-        return studentServiceImpl.findAllStudents();
+    public List<StudentDto> getAllStudents() {
+        List<Student> studentList = studentServiceImpl.findAllStudents();
+        return studentList.stream().map(this::convertToDto).collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
-    public Student getStudent(@PathVariable("id") String id) {
-        return studentServiceImpl.findByID(id);
+    public StudentDto getStudent(@PathVariable("id") String id) {
+        return convertToDto(studentServiceImpl.findByID(id));
     }
 
     @PostMapping
-    public void createStudent(Student student) {
+    public StudentDto createStudent(StudentDto studentDto) {
+        Student student = convertToEntity(studentDto);
+        studentServiceImpl.save(student);
+        return convertToDto(student);
+    }
+
+    @PutMapping
+    public void updateStudent(StudentDto studentDto) {
+        Student student = convertToEntity(studentDto);
         studentServiceImpl.save(student);
     }
 
